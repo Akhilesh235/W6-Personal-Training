7e385e571d924b05938dfcf78ea556f96561469a
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-10 23:14:28 +0800

    Creating A Class + Constructor + Destructor

--- W6 Personal Training/Program.cs
+++ W6 Personal Training/Program.cs
@@ -6,10 +6,34 @@ using System.Threading.Tasks;
 
 namespace W6_Personal_Training
 {
+    class Customer
+    {
+        string _firstName;  //creating string object
+        string _lastName;
+
+        public Customer(string FirstName, string LastName)  //constructor --> to initialize class field
+        {
+            this._firstName = FirstName;
+            this._lastName = LastName;
+        }
+
+        public void PrintFullName() //method 
+        {
+            Console.WriteLine("Full name : {0}" , this._firstName + " " + this._lastName);
+        }
+        ~Customer()
+        {
+            // clean up code goes here
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
+            Customer customer = new Customer("Akhilesh", "Dayal"); // creating instance (aka OBJECT) of the class
+            customer.PrintFullName();
+            Console.ReadLine();
         }
     }
 }
