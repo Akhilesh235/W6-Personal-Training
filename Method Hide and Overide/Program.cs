c6a685ccd0c0ad5a04fdf39c78e8070702a167cb
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-16 21:09:33 +0800

    Example of Method Override

---
+++ Method Hide and Overide/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Hide_and_Overide
+{
+    public class BaseClass
+    {
+        public virtual void Print()
+        { 
+            Console.WriteLine("I am from Base Class"); 
+        }
+    }
+
+    public class DerivedClass1 : BaseClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("I am from Derived Class 1");
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass BC = new DerivedClass1();
+            BC.Print();
+            Console.ReadLine();
+        }
+    }
+}
