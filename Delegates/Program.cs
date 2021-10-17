0136ea1c81ee815ea7e83678d48009fd88000c30
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-18 01:34:56 +0800

    Multi Cast Delegate v1.0

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -6,46 +6,39 @@ using System.Threading.Tasks;
 
 namespace Delegates
 {
+    public delegate void SampleDelegate();
     class Program
     {
         static void Main(string[] args)
         {
-            List<Employee> empList = new List<Employee>();
-            empList.Add(new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5 });
-            empList.Add(new Employee() { ID = 101, Name = "Mike", Salary = 4000, Experience = 4 });
-            empList.Add(new Employee() { ID = 101, Name = "John", Salary = 6000, Experience = 6 });
-            empList.Add(new Employee() { ID = 101, Name = "Todd", Salary = 3000, Experience = 3 });
 
-            
-            
-            
-            Employee.PromoteEmployee(empList, emp => emp.Experience >= 5);
+            SampleDelegate del1, del2, del3, del4;
+
+            del1 = new SampleDelegate(SampleMethodOne);
+            del2 = new SampleDelegate(SampleMethodTwo);
+            del3 = new SampleDelegate(SampleMethodThree);
+
+            del4 = del1 + del2 + del3;
+            del4(); // del 4 is the multi cast delegate
             Console.ReadLine();
         }
 
-        
-    }
-
-    delegate bool Ispromotable(Employee empl);
-
-    class Employee
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Salary { get; set; }
-        public int Experience { get; set; }
-
-        public static void PromoteEmployee(List<Employee> employeeList, Ispromotable IsEligibleToPromote)
+        public static void SampleMethodOne()
         {
-            foreach(Employee employee in employeeList)
-            {
-                if (IsEligibleToPromote(employee))
-                {
-                    Console.WriteLine(employee.Name + " promoted");
-                }
-                    
-            }
+            Console.WriteLine("Sample Method 1 Invoked");
+        }
+
+        public static void SampleMethodTwo()
+        {
+            Console.WriteLine("Sample Method 2 Invoked");
+        }
+
+        public static void SampleMethodThree()
+        {
+            Console.WriteLine("Sample Method 3 Invoked");
         }
 
     }
+
+ 
 }
