7040dd72b283194b99e386c82011ac321ffbbfba
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-17 22:20:58 +0800

    Creating abstract Class and using a derived Class to implement abstract method

---
+++ Abstract Classes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes
+{
+    public abstract class Customer
+    {
+        public abstract void Print();   // only declaration.. no implementation for abstract methods
+
+        
+    }
+    
+    class Program : Customer
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Print method");
+        }
+        static void Main(string[] args)
+        {
+            Customer C = new Program();
+            C.Print();
+            Console.ReadLine();
+        }
+    }
+}
